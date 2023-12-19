1d7dbf2b925b0e87ab04871824094aea3e90ee71
jeff-dude <jeff@dune.xyz>
2023-12-19 16:25:26 -0500

    exclude balances, process has changed how to build

--- models/_sector/tokens/ethereum/tokens_ethereum_base_balances_native.sql
+++ models/_sector/tokens/ethereum/tokens_ethereum_base_balances_native.sql
@@ -1,13 +1,14 @@
 {{ config(
-        schema = 'tokens_ethereum',
-        alias = 'base_balances_native',
-        partition_by = ['block_date'],
-        materialized = 'incremental',
-        file_format = 'delta',
-        incremental_strategy = 'merge',
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-        unique_key = ['block_number', 'tx_index', 'token_address', 'wallet_address'],
-        )
+    schema = 'tokens_ethereum',
+    alias = 'base_balances_native',
+    partition_by = ['block_date'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    unique_key = ['block_number', 'tx_index', 'token_address', 'wallet_address'],
+    tags = ['prod_exclude']
+    )
 }}
 
 select *
